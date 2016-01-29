6d4b8a7851816ce61b52bb0aa18fc9ed8d6e6e77
bczernecki <bczernecki@gmail.com>
2016-01-29 23:13:12 +0100

    fast update

--- R/meteorogram.R
+++ R/meteorogram.R
@@ -1,16 +1,22 @@
 
 # creating sample datasets:
+dates <- seq(ISOdatetime(2016,1,1,0,0,0, tz="UTC"), ISOdatetime(2016,1,3,0,0,0, tz="UTC"), "hours")
 
-<<<<<<< HEAD
+temp <- sin(1:length(dates)/pi)
+temp2 <- jitter(sin(1:length(dates)/pi),factor = 400)
 # TODO
 
-
-
+plot(temp2, type='l')
+lines(temp)
 # creating layout (similarly as presented @ meteo.pl)
 #        left, right, bottom, top
 par(fig=c(0.10,0.90,0.70,0.95), new=F, mar = c(0, 0, 0, 0))
-plot(matrix(runif(6),3,2), xaxt='n', xlab='')
-linie(rhum850_dzien_ave,brejks2)
+plot(temp, xaxt='n', xlab='', type='l', col='blue', lwd=2, ylim=c(range(c(temp,temp2))))
+lines(temp2, type='l', col='red', lwd=1.5)
+grid(nx = length(dates)/3, ny=0, col="black")
+  
+
+
 par(fig=c(0.47,0.93,0.67,1.00), new=TRUE)
 rysowanie_bezlinii(rhum850_noc_ave,brejks,tytul="")
 linie(rhum850_noc_ave,brejks2)
