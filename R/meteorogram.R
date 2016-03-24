537feda984f10b5277f93a43cae96b92f4a7b388
bczernecki <bczernecki@gmail.com>
2016-03-25 00:24:07 +0100

    @ changing from ASCII.txt to poznan.txt

--- R/meteorogram.R
+++ R/meteorogram.R
@@ -2,19 +2,23 @@ library(StreamMetabolism)
 library(plyr)
 
 # read sample WRF dataset
-dataset <- read.csv("http://openmeteo.pl/meta/wrf/ASCII.txt", sep=";", na.strings="-999000000.00 ")
+dataset <- read.csv("http://openmeteo.pl/meta/wrf/poznan.txt", sep=";", na.strings=c("-999000000.00 ","  Entire grid contents are set to missing data "))
 dataset$czas <- sub("Z"," ",as.character(dataset$czas))
 dataset$czas <- sub("JAN","-01-",as.character(dataset$czas))
 dataset$czas <- sub("FEB","-02-",as.character(dataset$czas))
+dataset$czas <- sub("MAR","-03-",as.character(dataset$czas))
 dataset$czas <- as.POSIXlt(strptime(dataset$czas, "%H %d-%m-%Y", tz="UTC"))
 
 # creating sample datasets:
 #dates <- seq(ISOdatetime(2016,1,29,1,0,0, tz="UTC"), ISOdatetime(2016,2,1,0,0,0, tz="UTC"), "hours")
-dates <- dataset$czas
+
+DF1 <- tail(dataset, n = 72)
+
+dates <- DF1$czas
 len <- length(dates)
 
-temp <- dataset$temp
-temp2 <- dataset$dpt2m
+temp <- DF1$temp
+temp2 <-DF1$dpt2m
 # TODO
 
 
@@ -59,9 +63,11 @@ par(fig=c(0.10,0.90,0.03,0.95), new=TRUE, mar = c(0, 0, 0, 0))
 for (i in 1:4) polygon(x = c(daynight$sunset[i], daynight$sunset[i], daynight$sunrise[i+1], daynight$sunrise[i+1]),  y=c(-40,50,50,-40 ), col=rgb(0.1,0.1,0.1,0.15), border = NA)
 
 # dodanie stopki:
-par(fig=c(0.10,0.90,0.00,1), new=TRUE, mar = c(0, 0, 0, 0))
-text(max(dates), y=-2,"(c) Zakład Klimatologii UAM (2016)", cex=0.6, pos=2)
-text(max(dates), y=-2.5,"Bartosz Czernecki & Mateusz Taszarek", cex=0.6, pos=2)
+#        left, right, bottom, top
+par(fig=c(0.10,0.90,0.13,1), new=T, mar = c(0, 0, 0, 0))
+#plot(1:100)
+mtext("(c) Zakład Klimatologii UAM (2016)",side=1, cex=0.6, padj=2)
+mtext("Bartosz Czernecki & Mateusz Taszarek", side=1,cex=0.6, padj=4)
 
 
 
@@ -70,7 +76,7 @@ text(max(dates), y=-2.5,"Bartosz Czernecki & Mateusz Taszarek", cex=0.6, pos=2)
 par(fig=c(0.10,0.90,0.50,0.67), new=TRUE, mar = c(0, 0, 0, 0))
 a <-sin(1:72)# wygenerowanie sztucznej serii
 x <- dates
-y <- dataset$opad
+y <- a
 if(is.na(y[1])) y[1]=0
 #x <- seq_along(y)
 y2 <- rep(y, each=2)
@@ -85,20 +91,20 @@ plot(x, y, ylim=c(0, max(y)+1), type="n", xaxs = "i", xaxt='n',  cex.axis=0.8)
 
 polygon(x3, y3, border=NA, col="#0000FF50", yaxs = "i", xaxs='i')
 lines(x2, y2)
-lines(dates,dataset$dpt2m/dataset$temp, xaxt='n', xlab='', type='l', col='coral', lwd=2)
+lines(dates,DF1$dpt2m/DF1$temp, xaxt='n', xlab='', type='l', col='coral', lwd=2)
 abline(h=c(0,0.5,1,2,5,10), lty=3)
 abline(v =seq(dates[1],max(dates), by="6 hour"), col="black", lty=3)
 
 
 par(fig=c(0.10,0.90,0.30,0.47), new=TRUE, mar = c(0, 0, 0, 0))
-slp_range <- round(range(dataset$cisn_msl))
+slp_range <- round(range(DF1$cisn_msl))
 slp_range[1] <- slp_range[1]-6
 slp_range[2] <- slp_range[2]+6
-barplot(dataset$cisn_msl, ylim=slp_range, col=add_slp_color(76),names.arg = dates, xaxt='n', yaxs='i',xaxs='i', border=NA, cex.axis=0.8,space = 0.0)
+barplot(DF1$cisn_msl, ylim=slp_range, col=add_slp_color(76),names.arg = dates, xaxt='n', yaxs='i',xaxs='i', border=NA, cex.axis=0.8,space = 0.0)
 box()
 
 x_max <- length(dates)-1
-lines(x = seq(0,x_max+1, length.out=x_max+1) ,dataset$cisn_msl,lty=1,col="black",xaxs='i', lwd=3)
+lines(x = seq(0,x_max+1, length.out=x_max+1) ,DF1$cisn_msl,lty=1,col="black",xaxs='i', lwd=3)
 abline(h=1:240*5, lty=3)
 abline(v =0:12*6.08, col="black", lty=3)
 
