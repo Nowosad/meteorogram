fa925aceb478c28baeb3788432adcede0046acb5
Your Name <bczernecki@gmail.com>
2016-10-17 00:01:25 +0200

    poprawki do bita/gita

--- R/running_loop.R
+++ R/running_loop.R
@@ -1,21 +1,20 @@
 # run entire procedure:
+bit <- "bitbucket"
 
-setwd("/home/bartosz/github/meteorogram/")
+setwd(paste0("/home/bartosz/",bit,"/meteorogram/"))
 source("R/read_gfs_from_file.R")
 source("R/add_slp_color.R")
 source("R/meteorogram2.R")
 
 for (lon1 in seq(from=14,to=25,by=0.25)){
   for(lat1 in seq(from=49,to=55,by=0.25)){
-    
-    cat(paste(lon1,lat1,"\n"))
-    
     DF1 <- create_df(gfs, lon1=lon1, lat1=lat1)
     tytul <- paste0(format(lon1, nsmall = 2),"_",format(lat1, nsmall = 2),".png")
     create_png(DF1 = DF1,tytul = tytul,lon1=lon1, lat1=lat1)
   }
+  cat(paste(lon1,lat1,"\n"))
 }
 
 system(command = "ls *png | wc", intern = T)
-system(command = "scp /home/bartosz/github/meteorogram/*png bartosz@openmeteo.pl:/var/www/meteorogram/")
+system(command = paste0("scp /home/bartosz/",bit,"/meteorogram/*png bartosz@openmeteo.pl:/var/www/meteorogram/"))
 system(command = "rm *png")
